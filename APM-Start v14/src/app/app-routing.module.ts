e1f43fe3209041f1dbe84150f5c87a5843b3ca02
Yuanheng Li <Yuanheng.Li@skolverket.se>
2023-04-13 16:18:58 +0200

    make routes dictionary as a const

--- APM-Start v14/src/app/app-routing.module.ts
+++ APM-Start v14/src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
+
+// this is the routes dictionary
+const ROUTES: Routes = [
+    { path: 'welcome', component: WelcomeComponent },
+    // products url is handled by the product module
+    // { path: 'products', component: ProductListComponent },
+    { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    { path: '**', component: PageNotFoundComponent }
+];
+
 @NgModule({
     imports: [
+        RouterModule.forRoot(ROUTES),
+        /*
         RouterModule.forRoot([
             { path: 'welcome', component: WelcomeComponent },
             // products url is handled by the product module
@@ -11,6 +23,7 @@ import { PageNotFoundComponent } from './page-not-found.component';
             { path: '', redirectTo: 'welcome', pathMatch: 'full' },
             { path: '**', component: PageNotFoundComponent }
         ]),
+        */
     ],
     exports: [RouterModule]
 })
