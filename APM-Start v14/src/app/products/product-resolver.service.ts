4dfa088d4d0d0f6e478f9a6918c4a068e59ae77c
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2023-04-23 22:26:30 +0200

    change Resolve<Product> to Resovle<ProdoctResolved> to handle retriving data fron backend

--- APM-Start v14/src/app/products/product-resolver.service.ts
+++ APM-Start v14/src/app/products/product-resolver.service.ts
@@ -1,23 +1,32 @@
 import { Injectable } from '@angular/core';
-import { Product } from './product';
+import { Product, ProductResolved } from './product';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { ProductService } from './product.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductResolverService implements Resolve<Product>{
+export class ProductResolverService implements Resolve<ProductResolved>{
 
   constructor(private productService: ProductService) { }
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Product | Observable<Product> | Promise<Product> {
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): ProductResolved | Observable<ProductResolved> | Promise<ProductResolved> {
     const id = route.paramMap?.get('id');
     if(isNaN(Number(id))) {
       const msg = `Product id was not a number: ${id}`;
       console.error(msg);
-      throw new Error(msg);
+      return of({ product: null, error: msg });
     }
-    return this.productService.getProduct(Number(id));
+
+    return this.productService.getProduct(Number(id))
+    .pipe(
+      map(product => ({ product: product })),
+      catchError(error => {
+        const msg = `Retrieval error: ${error}`;
+        console.error(msg);
+        return of({ product: null, error: msg });
+      })
+    );
   }
 }
